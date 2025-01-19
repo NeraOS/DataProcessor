c8ef9bde30025a88ec5244f1ab52a24508c8c78e
Aren Elchinyan <a2.dev@yandex.ru>
2025-01-19 16:38:55 +0300

    Add example

---
+++ test.py
@@ -0,0 +1,8 @@
+from data_processor import DataProcessor
+
+processor = DataProcessor()
+processor.load_data('data.csv')
+processor.filter_data(lambda row: int(row['age']) > 25)
+processor.sort_data('name')
+print(processor.calculate_mean('salary'))
+processor.save_data('processed_data.csv')
